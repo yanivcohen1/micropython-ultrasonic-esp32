e5d991f339784500c9b9748ee130dd21525d7912
yanivcohen1 <yaniv.choen.1@gmail.com>
2021-09-09 00:31:04 +0300

    Update net.py

--- user_lib/net.py
+++ user_lib/net.py
@@ -6,7 +6,7 @@ sta_if = network.WLAN(network.STA_IF)
 if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect("HOTBOX-89BA-yaniv", decrypt(b'C\xfcC\xe10>\xf8\xc4i\x88Da?\xd4\x82\x86'))
+        sta_if.connect("ssid-name", decrypt(b'C\xfdC\xe10>\xf9\xc4i\x88Da?\xd4\x82\x86'))
         while not sta_if.isconnected():
             pass
 
